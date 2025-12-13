ce878e6d8c20a89fa16c6d2292d8ecd51c419632
dlzwtk <2020694283@qq.com>
2025-12-13 12:01:19 +0800

    lab1_challenge_2 over

--- kernel/machine/mtrap.c
+++ kernel/machine/mtrap.c
@@ -1,6 +1,7 @@
 #include "kernel/riscv.h"
 #include "kernel/process.h"
 #include "spike_interface/spike_utils.h"
+#include "util/string.h"
 
 static void handle_instruction_access_fault() { panic("Instruction access fault!"); }
 
@@ -25,6 +26,50 @@ static void handle_timer()
   write_csr(sip, SIP_SSIP);
 }
 
+char errorfile_path[8192], errorline_code[32768];
+struct stat error_stat;
+static void print_errorline()
+{
+  int i = 0;
+  uint64 mepc = read_csr(mepc);
+  for (i = 0; i < current->line_ind; i++)
+  {
+    if (mepc < current->line[i].addr) // find the error line
+    {
+      addr_line *error_line = current->line + i - 1;
+      strcpy(errorfile_path, current->dir[current->file[error_line->file].dir]);
+      strcat(errorfile_path, "/");
+      strcat(errorfile_path, current->file[error_line->file].file);
+      // read the code line and print
+      spike_file_t *file = spike_file_open(errorfile_path, O_RDONLY, 0);
+      spike_file_stat(file, &error_stat);
+      spike_file_read(file, errorline_code, error_stat.st_size);
+      int cnt = 0, j = 0;
+      while (j < error_stat.st_size)
+      {
+        int code_ind;
+        for (code_ind = j; code_ind < error_stat.st_size && errorline_code[code_ind] != '\n'; code_ind++)
+        {
+        }
+        if (cnt == error_line->line - 1)
+        {
+          errorline_code[code_ind] = '\0';
+          sprint("Runtime error at %s:%d\n", errorfile_path, error_line->line);
+          sprint("%s\n", errorline_code + j);
+          break;
+        }
+        else
+        {
+          j = 1 + code_ind;
+          cnt++;
+        }
+      }
+      spike_file_close(file);
+      break;
+    }
+  }
+}
+
 //
 // handle_mtrap calls a handling function according to the type of a machine mode interrupt (trap).
 //
@@ -37,11 +82,14 @@ void handle_mtrap()
     handle_timer();
     break;
   case CAUSE_FETCH_ACCESS:
+    print_errorline();
     handle_instruction_access_fault();
     break;
   case CAUSE_LOAD_ACCESS:
+    print_errorline();
     handle_load_access_fault();
   case CAUSE_STORE_ACCESS:
+    print_errorline();
     handle_store_access_fault();
     break;
   case CAUSE_ILLEGAL_INSTRUCTION:
@@ -49,13 +97,16 @@ void handle_mtrap()
     // interception, and finish lab1_2.
 
     // panic( "call handle_illegal_instruction to accomplish illegal instruction interception for lab1_2.\n" );
+    print_errorline();
     handle_illegal_instruction();
 
     break;
   case CAUSE_MISALIGNED_LOAD:
+    print_errorline();
     handle_misaligned_load();
     break;
   case CAUSE_MISALIGNED_STORE:
+    print_errorline();
     handle_misaligned_store();
     break;
 
--- kernel/process.h
+++ kernel/process.h
@@ -3,7 +3,8 @@
 
 #include "riscv.h"
 
-typedef struct trapframe_t {
+typedef struct trapframe_t
+{
   // space to store context (all common registers)
   /* offset:0   */ riscv_regs regs;
 
@@ -13,31 +14,39 @@ typedef struct trapframe_t {
   /* offset:256 */ uint64 kernel_trap;
   // saved user process counter
   /* offset:264 */ uint64 epc;
-}trapframe;
+} trapframe;
 
 // code file struct, including directory index and file name char pointer
-typedef struct {
-    uint64 dir; char *file;
+typedef struct
+{
+  uint64 dir;
+  char *file;
 } code_file;
 
 // address-line number-file name table
-typedef struct {
-    uint64 addr, line, file;
+typedef struct
+{
+  uint64 addr, line, file;
 } addr_line;
 
 // the extremely simple definition of process, used for begining labs of PKE
-typedef struct process_t {
+typedef struct process_t
+{
   // pointing to the stack used in trap handling.
   uint64 kstack;
   // trapframe storing the context of a (User mode) process.
-  trapframe* trapframe;
+  trapframe *trapframe;
 
   // added @lab1_challenge2
-  char *debugline; char **dir; code_file *file; addr_line *line; int line_ind;
-}process;
+  char *debugline; // 编译器生成的调试信息，包括文件名、行号、列号等信息
+  char **dir;
+  code_file *file;
+  addr_line *line;
+  int line_ind;
+} process;
 
-void switch_to(process*);
+void switch_to(process *);
 
-extern process* current;
+extern process *current;
 
 #endif
