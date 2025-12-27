2d6e0f15c2ad264aa21951bf142839b66873eca1
dlzwtk <2020694283@qq.com>
2025-12-27 12:42:32 +0800

    lab2_1 pass

--- kernel/strap.c
+++ kernel/strap.c
@@ -63,7 +63,16 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval)
     // virtual address that causes the page fault.
     // panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
 
-    map_pages(current->pagetable, ROUNDDOWN(stval, PGSIZE), PGSIZE, (uint64)alloc_page(), prot_to_type(PROT_READ | PROT_WRITE, 1));
+    uint64 sp = current->trapframe->regs.sp;
+    if (stval - sp < 32)
+    {
+      map_pages(current->pagetable, ROUNDDOWN(stval, PGSIZE), PGSIZE, (uint64)alloc_page(), prot_to_type(PROT_READ | PROT_WRITE, 1));
+    }
+    else
+    {
+      sprint("this address is not available!\n");
+      shutdown(-1);
+    }
 
     break;
   default:
