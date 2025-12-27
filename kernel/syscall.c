af1ab247a38c1fdd9b8cd6e814d94f25f6a36ce0
dlzwtk <2020694283@qq.com>
2025-12-27 17:32:56 +0800

    lab4_1 pass

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -12,6 +12,7 @@
 #include "util/functions.h"
 #include "pmm.h"
 #include "vmm.h"
+#include "vfs.h"
 #include "sched.h"
 #include "proc_file.h"
 
@@ -20,11 +21,12 @@
 //
 // implement the SYS_user_print syscall
 //
-ssize_t sys_user_print(const char* buf, size_t n) {
+ssize_t sys_user_print(const char *buf, size_t n)
+{
   // buf is now an address in user space of the given app's user stack,
   // so we have to transfer it into phisical address (kernel is running in direct mapping).
-  assert( current );
-  char* pa = (char*)user_va_to_pa((pagetable_t)(current->pagetable), (void*)buf);
+  assert(current);
+  char *pa = (char *)user_va_to_pa((pagetable_t)(current->pagetable), (void *)buf);
   sprint(pa);
   return 0;
 }
@@ -32,10 +34,11 @@ ssize_t sys_user_print(const char* buf, size_t n) {
 //
 // implement the SYS_user_exit syscall
 //
-ssize_t sys_user_exit(uint64 code) {
+ssize_t sys_user_exit(uint64 code)
+{
   sprint("User exit with code:%d.\n", code);
   // reclaim the current process, and reschedule. added @lab3_1
-  free_process( current );
+  free_process(current);
   schedule();
   return 0;
 }
@@ -43,15 +46,19 @@ ssize_t sys_user_exit(uint64 code) {
 //
 // maybe, the simplest implementation of malloc in the world ... added @lab2_2
 //
-uint64 sys_user_allocate_page() {
-  void* pa = alloc_page();
+uint64 sys_user_allocate_page()
+{
+  void *pa = alloc_page();
   uint64 va;
   // if there are previously reclaimed pages, use them first (this does not change the
   // size of the heap)
-  if (current->user_heap.free_pages_count > 0) {
-    va =  current->user_heap.free_pages_address[--current->user_heap.free_pages_count];
+  if (current->user_heap.free_pages_count > 0)
+  {
+    va = current->user_heap.free_pages_address[--current->user_heap.free_pages_count];
     assert(va < current->user_heap.heap_top);
-  } else {
+  }
+  else
+  {
     // otherwise, allocate a new page (this increases the size of the heap by one page)
     va = current->user_heap.heap_top;
     current->user_heap.heap_top += PGSIZE;
@@ -59,7 +66,7 @@ uint64 sys_user_allocate_page() {
     current->mapped_info[HEAP_SEGMENT].npages++;
   }
   user_vm_map((pagetable_t)current->pagetable, va, PGSIZE, (uint64)pa,
-         prot_to_type(PROT_WRITE | PROT_READ, 1));
+              prot_to_type(PROT_WRITE | PROT_READ, 1));
 
   return va;
 }
@@ -67,7 +74,8 @@ uint64 sys_user_allocate_page() {
 //
 // reclaim a page, indicated by "va". added @lab2_2
 //
-uint64 sys_user_free_page(uint64 va) {
+uint64 sys_user_free_page(uint64 va)
+{
   user_vm_unmap((pagetable_t)current->pagetable, va, PGSIZE, 1);
   // add the reclaimed page to the free page list
   current->user_heap.free_pages_address[current->user_heap.free_pages_count++] = va;
@@ -77,20 +85,24 @@ uint64 sys_user_free_page(uint64 va) {
 //
 // kerenl entry point of naive_fork
 //
-ssize_t sys_user_fork() {
+ssize_t sys_user_fork()
+{
   sprint("User call fork.\n");
-  return do_fork( current );
+  return do_fork(current);
 }
 
 //
 // kerenl entry point of yield. added @lab3_2
 //
-ssize_t sys_user_yield() {
+ssize_t sys_user_yield()
+{
   // TODO (lab3_2): implment the syscall of yield.
   // hint: the functionality of yield is to give up the processor. therefore,
   // we should set the status of currently running process to READY, insert it in
   // the rear of ready queue, and finally, schedule a READY process to run.
   // panic( "You need to implement the yield syscall in lab3_2.\n" );
+
+  // sprint("User call yield");
   current->status = READY;
   insert_to_ready_queue(current);
   schedule();
@@ -100,23 +112,30 @@ ssize_t sys_user_yield() {
 //
 // open file
 //
-ssize_t sys_user_open(char *pathva, int flags) {
-  char* pathpa = (char*)user_va_to_pa((pagetable_t)(current->pagetable), pathva);
-  return do_open(pathpa, flags);
+ssize_t sys_user_open(char *pathva, int flags)
+{
+  char apath[MAX_PATH_LEN * 2];
+  char *pathpa = (char *)user_va_to_pa((pagetable_t)(current->pagetable), pathva);
+  relative_path_to_absolute_path(pathpa, apath);
+  return do_open(apath, flags);
 }
 
 //
 // read file
 //
-ssize_t sys_user_read(int fd, char *bufva, uint64 count) {
+ssize_t sys_user_read(int fd, char *bufva, uint64 count)
+{
   int i = 0;
-  while (i < count) { // count can be greater than page size
+  while (i < count)
+  { // count can be greater than page size
     uint64 addr = (uint64)bufva + i;
     uint64 pa = lookup_pa((pagetable_t)current->pagetable, addr);
     uint64 off = addr - ROUNDDOWN(addr, PGSIZE);
     uint64 len = count - i < PGSIZE - off ? count - i : PGSIZE - off;
     uint64 r = do_read(fd, (char *)pa + off, len);
-    i += r; if (r < len) return i;
+    i += r;
+    if (r < len)
+      return i;
   }
   return count;
 }
@@ -124,15 +143,19 @@ ssize_t sys_user_read(int fd, char *bufva, uint64 count) {
 //
 // write file
 //
-ssize_t sys_user_write(int fd, char *bufva, uint64 count) {
+ssize_t sys_user_write(int fd, char *bufva, uint64 count)
+{
   int i = 0;
-  while (i < count) { // count can be greater than page size
+  while (i < count)
+  { // count can be greater than page size
     uint64 addr = (uint64)bufva + i;
     uint64 pa = lookup_pa((pagetable_t)current->pagetable, addr);
     uint64 off = addr - ROUNDDOWN(addr, PGSIZE);
     uint64 len = count - i < PGSIZE - off ? count - i : PGSIZE - off;
     uint64 r = do_write(fd, (char *)pa + off, len);
-    i += r; if (r < len) return i;
+    i += r;
+    if (r < len)
+      return i;
   }
   return count;
 }
@@ -140,130 +163,236 @@ ssize_t sys_user_write(int fd, char *bufva, uint64 count) {
 //
 // lseek file
 //
-ssize_t sys_user_lseek(int fd, int offset, int whence) {
+ssize_t sys_user_lseek(int fd, int offset, int whence)
+{
   return do_lseek(fd, offset, whence);
 }
 
 //
 // read vinode
 //
-ssize_t sys_user_stat(int fd, struct istat *istat) {
-  struct istat * pistat = (struct istat *)user_va_to_pa((pagetable_t)(current->pagetable), istat);
+ssize_t sys_user_stat(int fd, struct istat *istat)
+{
+  struct istat *pistat = (struct istat *)user_va_to_pa((pagetable_t)(current->pagetable), istat);
   return do_stat(fd, pistat);
 }
 
 //
 // read disk inode
 //
-ssize_t sys_user_disk_stat(int fd, struct istat *istat) {
-  struct istat * pistat = (struct istat *)user_va_to_pa((pagetable_t)(current->pagetable), istat);
+ssize_t sys_user_disk_stat(int fd, struct istat *istat)
+{
+  struct istat *pistat = (struct istat *)user_va_to_pa((pagetable_t)(current->pagetable), istat);
   return do_disk_stat(fd, pistat);
 }
 
 //
 // close file
 //
-ssize_t sys_user_close(int fd) {
+ssize_t sys_user_close(int fd)
+{
   return do_close(fd);
 }
 
 //
 // lib call to opendir
 //
-ssize_t sys_user_opendir(char * pathva){
-  char * pathpa = (char*)user_va_to_pa((pagetable_t)(current->pagetable), pathva);
-  return do_opendir(pathpa);
+ssize_t sys_user_opendir(char *pathva)
+{
+  char apath[MAX_PATH_LEN * 2];
+  char *pathpa = (char *)user_va_to_pa((pagetable_t)(current->pagetable), pathva);
+  relative_path_to_absolute_path(pathpa, apath);
+  return do_opendir(apath);
 }
 
 //
 // lib call to readdir
 //
-ssize_t sys_user_readdir(int fd, struct dir *vdir){
-  struct dir * pdir = (struct dir *)user_va_to_pa((pagetable_t)(current->pagetable), vdir);
+ssize_t sys_user_readdir(int fd, struct dir *vdir)
+{
+  struct dir *pdir = (struct dir *)user_va_to_pa((pagetable_t)(current->pagetable), vdir);
   return do_readdir(fd, pdir);
 }
 
 //
 // lib call to mkdir
 //
-ssize_t sys_user_mkdir(char * pathva){
-  char * pathpa = (char*)user_va_to_pa((pagetable_t)(current->pagetable), pathva);
-  return do_mkdir(pathpa);
+ssize_t sys_user_mkdir(char *pathva)
+{
+  char apath[MAX_PATH_LEN * 2];
+  char *pathpa = (char *)user_va_to_pa((pagetable_t)(current->pagetable), pathva);
+  relative_path_to_absolute_path(pathpa, apath);
+  return do_mkdir(apath);
 }
 
 //
 // lib call to closedir
 //
-ssize_t sys_user_closedir(int fd){
+ssize_t sys_user_closedir(int fd)
+{
   return do_closedir(fd);
 }
 
 //
 // lib call to link
 //
-ssize_t sys_user_link(char * vfn1, char * vfn2){
-  char * pfn1 = (char*)user_va_to_pa((pagetable_t)(current->pagetable), (void*)vfn1);
-  char * pfn2 = (char*)user_va_to_pa((pagetable_t)(current->pagetable), (void*)vfn2);
+ssize_t sys_user_link(char *vfn1, char *vfn2)
+{
+  char *pfn1 = (char *)user_va_to_pa((pagetable_t)(current->pagetable), (void *)vfn1);
+  char *pfn2 = (char *)user_va_to_pa((pagetable_t)(current->pagetable), (void *)vfn2);
   return do_link(pfn1, pfn2);
 }
 
 //
 // lib call to unlink
 //
-ssize_t sys_user_unlink(char * vfn){
-  char * pfn = (char*)user_va_to_pa((pagetable_t)(current->pagetable), (void*)vfn);
+ssize_t sys_user_unlink(char *vfn)
+{
+  char *pfn = (char *)user_va_to_pa((pagetable_t)(current->pagetable), (void *)vfn);
   return do_unlink(pfn);
+}
+
+//
+// lib call to read cwd
+//
+ssize_t sys_user_rcwd(char *vpath)
+{
+  char *path = (char *)user_va_to_pa((pagetable_t)(current->pagetable), (void *)vpath);
+  return do_rcwd(path);
+}
+
+//
+// lib call to change cwd
+//
+ssize_t sys_user_ccwd(const char *vpath)
+{
+  char apath[MAX_PATH_LEN * 2];
+  char *path = (char *)user_va_to_pa((pagetable_t)(current->pagetable), (void *)vpath);
+  relative_path_to_absolute_path(path, apath);
+  return do_ccwd(apath);
+}
+
+void relative_path_to_absolute_path(char *relative_path, char *absolute_path)
+{
+  const char *rp = relative_path;
+  char *ap = absolute_path;
+  if (strlen(relative_path) == 0) // relative_path is empty
+  {
+    return;
+  }
+  else if (relative_path[0] == '/') // not a relative_path
+  {
+    return;
+  }
+  if (strcmp("/", current->pfiles->cwd->name) != 0)
+  {
+    strcpy(absolute_path, current->pfiles->cwd->name);
+  }
+  while (*ap != '\0')
+  {
+    ap++;
+  }
+  while (*rp != '\0')
+  {
+    char tmp[MAX_PATH_LEN * 2], *t = tmp;
+    while (*rp != '\0' && *rp != '/')
+    {
+      *t = *rp;
+      rp++;
+      t++;
+    }
+    *t = '\0';
+    // sprint("tmp is %s\n", tmp);
+    if (strcmp("..", tmp) == 0) // parse ../
+    {
+      ap--;
+      while (*ap != '/') // /
+      {
+        ap--;
+      }
+    }
+    else if (strcmp(".", tmp) != 0) // parse other
+    {
+      *ap = '/';
+      ap++;
+      t = tmp;
+      while (*t != '\0')
+      {
+        *ap = *t;
+        t++;
+        ap++;
+      }
+    }
+    if (*rp != '\0')
+    {
+      rp++;
+    }
+  }
+  *ap = '\0';
+  if (strlen(absolute_path) == 0)
+  {
+    strcpy(absolute_path, "/");
+  }
+  // sprint("relative_path is %s\n", relative_path);
+  // sprint("absolute_path is %s\n", absolute_path);
+  // strcpy(relative_path, absolute_path);
 }
 
 //
 // [a0]: the syscall number; [a1] ... [a7]: arguments to the syscalls.
 // returns the code of success, (e.g., 0 means success, fail for otherwise)
 //
-long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, long a7) {
-  switch (a0) {
-    case SYS_user_print:
-      return sys_user_print((const char*)a1, a2);
-    case SYS_user_exit:
-      return sys_user_exit(a1);
-    // added @lab2_2
-    case SYS_user_allocate_page:
-      return sys_user_allocate_page();
-    case SYS_user_free_page:
-      return sys_user_free_page(a1);
-    case SYS_user_fork:
-      return sys_user_fork();
-    case SYS_user_yield:
-      return sys_user_yield();
-    // added @lab4_1
-    case SYS_user_open:
-      return sys_user_open((char *)a1, a2);
-    case SYS_user_read:
-      return sys_user_read(a1, (char *)a2, a3);
-    case SYS_user_write:
-      return sys_user_write(a1, (char *)a2, a3);
-    case SYS_user_lseek:
-      return sys_user_lseek(a1, a2, a3);
-    case SYS_user_stat:
-      return sys_user_stat(a1, (struct istat *)a2);
-    case SYS_user_disk_stat:
-      return sys_user_disk_stat(a1, (struct istat *)a2);
-    case SYS_user_close:
-      return sys_user_close(a1);
-    // added @lab4_2
-    case SYS_user_opendir:
-      return sys_user_opendir((char *)a1);
-    case SYS_user_readdir:
-      return sys_user_readdir(a1, (struct dir *)a2);
-    case SYS_user_mkdir:
-      return sys_user_mkdir((char *)a1);
-    case SYS_user_closedir:
-      return sys_user_closedir(a1);
-    // added @lab4_3
-    case SYS_user_link:
-      return sys_user_link((char *)a1, (char *)a2);
-    case SYS_user_unlink:
-      return sys_user_unlink((char *)a1);
-    default:
-      panic("Unknown syscall %ld \n", a0);
+long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, long a7)
+{
+  switch (a0)
+  {
+  case SYS_user_print:
+    return sys_user_print((const char *)a1, a2);
+  case SYS_user_exit:
+    return sys_user_exit(a1);
+  // added @lab2_2
+  case SYS_user_allocate_page:
+    return sys_user_allocate_page();
+  case SYS_user_free_page:
+    return sys_user_free_page(a1);
+  case SYS_user_fork:
+    return sys_user_fork();
+  case SYS_user_yield:
+    return sys_user_yield();
+  // added @lab4_1
+  case SYS_user_open:
+    return sys_user_open((char *)a1, a2);
+  case SYS_user_read:
+    return sys_user_read(a1, (char *)a2, a3);
+  case SYS_user_write:
+    return sys_user_write(a1, (char *)a2, a3);
+  case SYS_user_lseek:
+    return sys_user_lseek(a1, a2, a3);
+  case SYS_user_stat:
+    return sys_user_stat(a1, (struct istat *)a2);
+  case SYS_user_disk_stat:
+    return sys_user_disk_stat(a1, (struct istat *)a2);
+  case SYS_user_close:
+    return sys_user_close(a1);
+  // added @lab4_2
+  case SYS_user_opendir:
+    return sys_user_opendir((char *)a1);
+  case SYS_user_readdir:
+    return sys_user_readdir(a1, (struct dir *)a2);
+  case SYS_user_mkdir:
+    return sys_user_mkdir((char *)a1);
+  case SYS_user_closedir:
+    return sys_user_closedir(a1);
+  // added @lab4_3
+  case SYS_user_link:
+    return sys_user_link((char *)a1, (char *)a2);
+  case SYS_user_unlink:
+    return sys_user_unlink((char *)a1);
+  case SYS_user_rcwd:
+    return sys_user_rcwd((char *)a1);
+  case SYS_user_ccwd:
+    return sys_user_ccwd((const char *)a1);
+  default:
+    panic("Unknown syscall %ld \n", a0);
   }
 }
